d0f057e42e2c4c9553f15ba33c9c331be589ecfe
jeremy <jeremyandika21@gmail.com>
2023-05-23 01:18:26 +0700

    add hero styling to resolve #1

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import bitcoin from '../assets/bitcoin.png'
+import eth from '../assets/eth.png'
+import t from '../assets/3.png'
+import s from '../assets/s.png'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div>
+      <div className='hero'><h1>STAY AHEAD OF THE <span className='crypto-text'>CRYPTO</span> GAME</h1></div>
+      <div className='img-hero'>
+        <img src={bitcoin} alt='bitcoin' ></img>
+        <img src={eth} alt='eth' ></img>
+        <img src={t} alt='t' ></img>
+        <img src={s} alt='s' ></img>
+      </div>
+    </div>
   )
 }
 
