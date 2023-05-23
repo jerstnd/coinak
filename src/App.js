a640481e17f84e0872c567647ce23d782bdcfccc
jeremy <jeremyandika21@gmail.com>
2023-05-24 06:57:55 +0700

    making some changes in styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from './Components/Navbar';
-import Hero from './Components/Hero'
+import Hero from './Components/Hero';
+import CoinList from './Components/CoinList'
 
 function App() {
+  
   return (
+    <div>
     <div className="main">
         <Navbar />
         <Hero />
+    </div>
+    <div className='market'>
+    <CoinList />
+    </div>
     </div>
   );
 }
---
+++ src/Components/CoinList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function CoinList() {
+    return (
+    <div>
+      <div className='market-title'>Market Update</div>
+      <div className='market-content'>
+        <div className='market-content-title'>
+          <p>Coin</p>
+          <p>Price</p>
+          <p>24h Change</p>
+          <p>Market Cap</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CoinList
