229515aaed192cc3608bc6087f89da3533ae95f9
jeremy <jeremyandika21@gmail.com>
2023-06-04 16:16:04 +0700

    add register functionality

--- src/Screens/RegisterScreen.js
+++ src/Screens/RegisterScreen.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Link} from 'react-router-dom'
+import AuthContext from '../context/AuthContext'
 
 function RegisterScreen() {
+  let {registerUser} = useContext(AuthContext)
+
   return (
     <div className='register'>
     <Link to={"/login"}><button className='btn-back-login'>Go Back</button></Link>
@@ -11,7 +14,7 @@ function RegisterScreen() {
         <h2>REGISTER</h2>
         <div className="underline-title"></div>
       </div>
-      <form method="post" className="form">
+      <form method="post" className="form" onSubmit={registerUser}>
         <label for="user-email" style={{paddingTop:"13px"}}>
             &nbsp;Username
           </label>
@@ -23,7 +26,7 @@ function RegisterScreen() {
         <div className="form-border"></div>
         <label for="user-password" style={{paddingTop:"22px"}}>&nbsp;Confirm Password
           </label>
-        <input id="user-password" className="form-content" type="password" name="password" required />
+        <input id="user-password2" className="form-content" type="password" name="password2" required />
         <div className="form-border"></div>
         <input id="submit-btn" type="submit" name="submit" value="REGISTER" />
       </form>
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -37,6 +37,23 @@ export const AuthProvider = ({children}) => {
         }
     }
 
+    let registerUser = async (e) => {
+        e.preventDefault()
+        let response = await fetch('http://127.0.0.1:8000/api/register/', {
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'username':e.target.username.value, 'password':e.target.password.value, 'password2':e.target.password2.value})
+        })
+
+        if(response.status === 201){
+            navigate('/login')
+        }else{
+            alert('Something went wrong')
+        }
+    }
+
     let logoutUser = () => {
         setAuthToken(null)
         setUser(null)
@@ -45,7 +62,6 @@ export const AuthProvider = ({children}) => {
     }
 
     let updateToken = async (e) => {
-        console.log('UpdateToken called')
         let response = await fetch('http://127.0.0.1:8000/api/token/refresh/', {
             method: 'POST',
             headers:{
@@ -68,10 +84,10 @@ export const AuthProvider = ({children}) => {
         user:user,
         loginUser:loginUser,
         logoutUser:logoutUser,
+        registerUser:registerUser,
     }
 
     useEffect(()=> {
-
         let fourMinutes = 1000 * 60 * 4
         let interval = setInterval(() =>{
             if(authToken){
